e96faa55fd80f3254de7d76918711f7f989cd01e
kevin ochiengg <kevinohkelvin9@gmail.com>
2025-07-23 13:37:57 +0300

    fixed a missing import express from 'express'

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,4 +1,4 @@
-// import express from 'express'
+import express from 'express'
 import multer from 'multer'
 import { CloudinaryStorage } from 'multer-storage-cloudinary'
 import { v2 as cloudinary } from 'cloudinary'
