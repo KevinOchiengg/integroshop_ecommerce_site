3603a54a578e5fc8038919e430be0ae72f15bdf0
kevin ochiengg <kevinohkelvin9@gmail.com>
2025-08-01 10:51:19 +0300

    removed payment with paypal logic

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/integroshops',
   GOOGLE_API_key: process.env.GOOGLE_API_key,
-   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  //  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
   secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
   MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN || 'ko',
